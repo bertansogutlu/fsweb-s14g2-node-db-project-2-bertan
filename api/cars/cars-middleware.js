2b03d1526f34631c8b79530b31e8dfa301546b99
Bertan Sogutlu <bertansogutlu@gmail.com>
2023-03-30 04:24:51 +0300

    server olusturuldu

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -8,7 +8,6 @@ const checkCarId = async(req, res, next) => {
     if(!car){
       res.status(404).json({ mesaj: `${req.params.id} kimliğine sahip araba bulunamadı` });
     } else {
-      res.car = car;
       next();
     }
   } catch (error) {
@@ -19,18 +18,18 @@ const checkCarId = async(req, res, next) => {
 const checkCarPayload = (req, res, next) => {
   // HOKUS POKUS
   try {
-    const {vin,make,model,milage} = req.body;
+    const {vin,make,model,mileage} = req.body;
     if(vin === undefined) {
-      res.status(400).json({ mesaj: `vin eksik` });
+      res.status(400).json({ message: `vin is missing` });
     }
     else if(make === undefined) {
-      res.status(400).json({ mesaj: `make eksik` });
+      res.status(400).json({ message: `make is missing` });
     }
     else if(model === undefined) {
-      res.status(400).json({ mesaj: `model eksik` });
+      res.status(400).json({ message: `model is missing` });
     }
-    else if(milage === undefined) {
-      res.status(400).json({ mesaj: `milage eksik` });
+    else if(mileage === undefined) {
+      res.status(400).json({ message: `mileage is missing` });
     } else {
       next();
     }
@@ -44,7 +43,7 @@ const checkVinNumberValid = (req, res, next) => {
   try {
     const isValidVin = vinValidator.validate(req.body.vin);
     if(!isValidVin){
-      res.status(400).json({ mesaj: `vin ${req.body.vin} geçersizdir` });
+      res.status(400).json({ message: `vin ${req.body.vin} is invalid` });
     } else{
       next();
     }
@@ -58,7 +57,7 @@ const checkVinNumberUnique = async(req, res, next) => {
   try {
     const isExistVin = await carsModel.getByVinNumber(req.body.vin);
     if(isExistVin){
-      res.status(400).json({ mesaj: `vin ${req.body.vin} zaten var` });
+      res.status(400).json({ message: `vin ${req.body.vin} already exists` });
     } else{
       next();
     }
--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -2,20 +2,21 @@
 const express = require("express");
 const router = express.Router();
 const middleware = require("./cars-middleware");
-const carModel = require("./cars-model");
+const carsModel = require("./cars-model");
 
 router.get("/", async (req, res, next) => {
   try {
-    const allCars = carModel.getAll();
+    const allCars = await carsModel.getAll();
     res.status(200).json(allCars);
   } catch (error) {
     next(error);
   }
 });
 
-router.get("/:id", middleware.checkCarId, (req, res, next) => {
+router.get("/:id", middleware.checkCarId, async(req, res, next) => {
   try {
-    res.status(200).json(req.car);
+    const car = await carsModel.getById(req.params.id);
+    res.status(200).json(car);
   } catch (error) {
     next(error);
   }
@@ -23,8 +24,8 @@ router.get("/:id", middleware.checkCarId, (req, res, next) => {
 
 router.post("/", middleware.checkCarPayload, middleware.checkVinNumberValid, middleware.checkVinNumberUnique, async (req, res, next) => {
     try {
-    const insertedCar = await carModel.create(req.body)
-      res.status(200).json(rinsertedCar);
+    const insertedCar = await carsModel.create(req.body)
+      res.status(200).json(insertedCar);
     } catch (error) {
       next(error);
     }
